3c661405bc8a296ecf04ffb19baea06efbafbc14
Albert <algoam@gmail.com>
2025-08-17 12:20:29 +0200

    Match access challenge view to Reptes

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js');
 
 // This value is replaced at build time by tools/update_sw_version.py
-const CACHE_VERSION = '634731946db1';
+const CACHE_VERSION = '7279f022de4f';
 
 self.addEventListener('install', () => self.skipWaiting());
 
