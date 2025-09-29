099f9ddda228019f883ab28b24f142b5e2f28e89
Albert <algoam@gmail.com>
2025-09-29 17:17:10 +0200

    Add iframe experiment for championship view

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { inicialitza } from './js/init.js';
 import { setupRouter } from './js/router.js';
 import { closeChart, handleResize } from './js/graficos.js';
 import { setupInstallPrompt } from './js/install.js';
+import { setupIframeExperiment } from './js/iframe-experiment.js';
 
 setupServiceWorker();
 setupInstallPrompt();
@@ -11,4 +12,5 @@ setupRouter();
 document.getElementById('close-chart').addEventListener('click', closeChart);
 window.addEventListener('resize', handleResize);
 
+setupIframeExperiment();
 inicialitza();
